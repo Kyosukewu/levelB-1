026922c60a2f8afcc2840fc7669f636af98143f1
Kyosukewu <kyosukewu2020@gmail.com>
2020-12-14 14:15:36 +0800

    完成更多消息顯示區詳細顯示

--- news.php
+++ news.php
@@ -7,44 +7,46 @@
 				<span class="t botli">更多最新消息顯示區</span>
 
 				<?php
-					$all=$News->count(['sh'=>1]);
-					$div=5;
-					$pages=ceil($all/$div);
-					$now=(isset($_GET['p']))?$_GET['p']:1;
-					// $now=(isset($_GET['p']))??1;  //短寫法
-					$start=($now-1)*$div;
+				$all = $News->count(['sh' => 1]);
+				$div = 5;
+				$pages = ceil($all / $div);
+				$now = (isset($_GET['p'])) ? $_GET['p'] : 1;
+				// $now=(isset($_GET['p']))??1;  //短寫法
+				$start = ($now - 1) * $div;
 				?>
 
-				<ol start="<?=$start+1;?>">
+				<ol start="<?= $start + 1; ?>">
 					<?php
-					foreach ($News->all(['sh' => 1]," limit $start,$div") as $key => $new) {
+					foreach ($News->all(['sh' => 1], " limit $start,$div") as $key => $new) {
 					?>
-					<li class="sswww" style="margin:3px;"><?=mb_substr($new['text'],0,25);?></li>
-					<li class="all" style="display:none;"><?= $new['text']; ?></li>
+						<li class="sswww" style="margin:3px;"><?= mb_substr($new['text'], 0, 25); ?>
+							<div class="all" style="display:none;"><?= $new['text']; ?></div>
+						</li>
+
 					<?php
 					}
 					?>
 				</ol>
 				<div style="text-align:center;">
-				<?php
-					if(($now-1)>0){
-				?>
-					<a class="bl" style="font-size:30px;" href="?do=news&p=<?=$now-1;?>">&lt;&nbsp;</a>
+					<?php
+					if (($now - 1) > 0) {
+					?>
+						<a class="bl" style="font-size:30px;" href="?do=news&p=<?= $now - 1; ?>">&lt;&nbsp;</a>
 					<?php
 					}
-						for($i=1;$i<=$pages;$i++){
-							if($i==$now){
-								$font="3.5rem";
-							}else{
-								$font="30px";
-							}
-							echo "<a style='font-size:$font;' href='?do=news&p=$i'> ";
-							echo $i;
-							echo " </a>";
+					for ($i = 1; $i <= $pages; $i++) {
+						if ($i == $now) {
+							$font = "3.5rem";
+						} else {
+							$font = "30px";
 						}
-					if(($now+1)<=$pages){
+						echo "<a style='font-size:$font;' href='?do=news&p=$i'> ";
+						echo $i;
+						echo " </a>";
+					}
+					if (($now + 1) <= $pages) {
 					?>
-					<a class="bl" style="font-size:30px;" href="?do=news&p=<?=$now+1;?>">&nbsp;&gt;</a>
+						<a class="bl" style="font-size:30px;" href="?do=news&p=<?= $now + 1; ?>">&nbsp;&gt;</a>
 					<?php
 					}
 					?>
