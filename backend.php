52a8444a7f27b999ccf164b86e337b7816fb3923
Kyosukewu <kyosukewu2020@gmail.com>
2020-12-14 13:55:57 +0800

    非法入侵阻擋

--- backend.php
+++ backend.php
@@ -1,5 +1,10 @@
 ﻿<?php
 include_once "base.php";
+if(empty($_SEESION['login'])){
+	echo "404 NOT FOUND";
+	// exit();
+	to("index.php");
+}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0068)?do=admin&redo=title -->
